f8144abfba000b4996d7b80507998a7714a3de39
William King <williamking9772@gmail.com>
2020-08-17 17:02:39 +0100

    Created the UI for the Login Page
    Needs functionality.

---
+++ src/LoginController.java
@@ -0,0 +1,29 @@
+/**
+ * Controller for the Login page.
+ * Handles actions performed by the login page.
+ * This includes checking users and setting up any children windows.
+ * @author William King
+ */
+public class LoginController {
+	
+	/**
+	 * Sets up the linked hashmap holding all the users.
+	 */
+	public void initialize() {
+		
+	}
+	
+	/**
+	 * Handles a button event for the login button e.g. button press.
+	 */
+	public void handleLoginButtonAction() {
+		
+	}
+	
+	/**
+	 * Displays a page that allows the user to create a new profile.
+	 */
+	public void handleCreateNewProfileButtonAction() {
+		
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+/**
+ * The main class starts the program and executes the 
+ * login page for use.
+ * @author William King
+ */
+public class Main extends Application {
+	/** The title given to the stage. */
+	private static final String STAGE_TITLE = "The Game";
+	/** Width of the stage. */
+	private static final int STAGE_WIDTH = 360;
+	/** Height of the stage. */
+	private static final int STAGE_HEIGHT = 176;
+	
+	/**
+	 * Performs any pre-launch tasks and then launches the system.
+	 * @param args Takes a string of arguments.
+	 */
+	public static void main(String[] args) {
+		// Run the program.
+		launch(args);
+	}
+	
+	/**
+	 * The primary stage hosts the application and provides a UI for the user.
+	 * @param primaryStage The stage.
+	 */
+	@Override 
+	public void start(Stage primaryStage) {
+		try {
+			// Load the main scene.
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("FXMLFiles/Login.fxml"));
+			Scene scene = new Scene(root, STAGE_WIDTH, STAGE_HEIGHT);
+			
+			// Place the main scene on stage and show it.
+			primaryStage.setScene(scene);
+			primaryStage.setTitle(STAGE_TITLE);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+}
